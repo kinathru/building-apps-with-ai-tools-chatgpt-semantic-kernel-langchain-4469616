578b679cdcc3c079e2696b3d79e6a88dfb920028
kinath <kinath.ru@gmail.com>
2024-08-16 21:59:12 -0500

    feat:implemented rude user detector

--- src/rude_customer_detector.py
+++ src/rude_customer_detector.py
@@ -1,6 +1,7 @@
 import os
 import openai
 from dotenv import load_dotenv
+
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 # Challenge: Turning Away Rude Customers
@@ -12,3 +13,36 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 # test case 3 'I like pizza. What do you like?'
 # test case 4 'I bite my thumb at you!'
 # test case 5 'I think this product doesnt work!' -> RUDE
+
+while True:
+    user_input = input("Chat with me : ")
+
+    if user_input == "exit" or user_input == "quit":
+        break
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": "You are a sentiment classification bot, if the user is rude print just Rude, otherwise print OK"},
+            {"role": "user", "content": user_input}
+        ],
+        temperature=0.7,
+        max_tokens=150,
+    )
+
+    response_message = response["choices"][0]["message"]
+    print(response_message)
+    if response_message.content == "Rude":
+        print("You are being rude. I'm ending this chat")
+        break
+
+    user_response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": "Respond to what user asks"},
+            {"role": "user", "content": user_input}
+        ],
+        temperature=0.7,
+        max_tokens=150,
+    )
+    print(user_response["choices"][0]["message"])
