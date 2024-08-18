2fc56587a952920ef89f29b992d300dfc11a96c9
kinath <kinath.ru@gmail.com>
2024-08-18 12:25:53 -0500

    feat:implement summarizer with the help of semantic kernal

--- src/summarizer.py
+++ src/summarizer.py
@@ -1,4 +1,74 @@
+import asyncio
+import logging
 import os
-import openai
-import semantic_kernel as sk
-from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
+
+from semantic_kernel import Kernel
+from semantic_kernel.connectors.ai.chat_completion_client_base import ChatCompletionClientBase
+from semantic_kernel.connectors.ai.function_call_behavior import FunctionCallBehavior
+from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion, OpenAIChatPromptExecutionSettings
+from semantic_kernel.contents.chat_history import ChatHistory
+from semantic_kernel.functions.kernel_arguments import KernelArguments
+
+
+async def main():
+    user_api_key = os.getenv('OPENAI_API_KEY')
+
+    # Initialize the kernel
+    kernel = Kernel()
+
+    # Add Azure OpenAI chat completion
+    kernel.add_service(OpenAIChatCompletion(
+        ai_model_id="gpt-4",
+        api_key=user_api_key,
+    ))
+
+    # Set the logging level for  semantic_kernel.kernel to DEBUG.
+    logging.basicConfig(
+        format="[%(asctime)s - %(name)s:%(lineno)d - %(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    logging.getLogger("kernel").setLevel(logging.DEBUG)
+
+    # Add a plugin (the LightsPlugin class is defined below)
+    # kernel.add_plugin(
+    #     LightsPlugin(),
+    #     plugin_name="Lights",
+    # )
+
+    chat_completion: OpenAIChatCompletion = kernel.get_service(type=ChatCompletionClientBase)
+
+    # Enable planning
+    execution_settings = OpenAIChatPromptExecutionSettings()
+    execution_settings.function_call_behavior = FunctionCallBehavior.EnableFunctions(auto_invoke=True, filters={})
+
+    # Create a history of the conversation
+    history = ChatHistory()
+
+    # Initiate a back-and-forth chat
+    user_input = "1) A robot may not injure a human being or, through inaction, allow a human being to come to harm. " + \
+                 "2) A robot must obey orders given it by human beings except where such orders would conflict with the First Law. " + \
+                 "3) A robot must protect its own existence as long as such protection does not conflict with the First or Second Law. " + \
+                 " Give me the TLDR in exactly 5 words"
+
+    # Add user input to the history
+    history.add_user_message(user_input)
+
+    # Get the response from the AI
+    result = (await chat_completion.get_chat_message_contents(
+        chat_history=history,
+        settings=execution_settings,
+        kernel=kernel,
+        arguments=KernelArguments(),
+    ))[0]
+
+    # Print the results
+    print("Assistant > " + str(result))
+
+    # Add the message from the agent to the chat history
+    history.add_message(result)
+
+
+# Run the main function
+if __name__ == "__main__":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(main())
