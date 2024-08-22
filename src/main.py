e38b9ab609bbe216ff9211d6c68bd576c758989f
kinath <kinath.ru@gmail.com>
2024-08-22 06:21:30 -0500

    feat:implement librarian logic with vector db

--- src/main.py
+++ src/main.py
@@ -1,24 +1,44 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts.chat import (
-    PromptTemplate
-)
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.llms import OpenAI
-from langchain.chains import RetrievalQA
-from langchain.vectorstores import Qdrant
-from langchain.output_parsers import PydanticOutputParser
-from qdrant_client.http import models as rest
-from pydantic import BaseModel, Field
-from langchain.document_loaders.csv_loader import CSVLoader
+import os
 
+from dotenv import load_dotenv
+from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_community.vectorstores import Qdrant
+from langchain_openai import OpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
 
-import csv
-from typing import Dict, List, Optional
-from langchain.document_loaders.base import BaseLoader
-from langchain.docstore.document import Document
+# Load the .env file
+load_dotenv()
 
+user_api_key = os.getenv('OPENAI_API_KEY')
 
-loader = CSVLoader(
-    file_path="./src/dataset_small.csv", source_column="title")
+# load content from wikipedia Tea page
+loader = CSVLoader(file_path="./dataset_small.csv")
+documents = loader.load()
 
-data = loader.load()
+print(documents)
+
+# Putting all data into ChatBot is not needed. So we split data into chunks.
+# Chunks are pieces of data that's split up
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(documents)
+
+# Embeddings is a numerical representation of the text
+embeddings = OpenAIEmbeddings()
+docsearch = Qdrant.from_documents(documents,
+                                  embeddings,
+                                  location=":memory:",
+                                  collection_name="book")
+
+# Here we are creating a Q&A retrieval chatbot, with OpenAI to power the chatbot which decides how to summarize the response from documents
+qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever(), return_source_documents=True)
+
+while True:
+    query = input("Ask a question about books : \n")
+
+    if query == 'exit':
+        break
+
+    # run Q&A
+    print(qa.invoke(query))
