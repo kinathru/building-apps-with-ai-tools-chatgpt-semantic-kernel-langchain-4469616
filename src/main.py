31c01b41541d02482a7719bd739668fa1268e27a
kinath <kinath.ru@gmail.com>
2024-08-23 22:14:58 -0500

    feat:generate realistic test data

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts.chat import (
+import os
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts.chat import (
     PromptTemplate
 )
 from langchain.output_parsers import PydanticOutputParser
@@ -14,3 +17,25 @@ class Person(BaseModel):
 
 class PeopleList(BaseModel):
     people: list[Person] = Field(description="A list of people")
+
+
+# Load the .env file
+load_dotenv()
+
+user_api_key = os.getenv('OPENAI_API_KEY')
+
+model = ChatOpenAI(model="gpt-4")
+people_data = model.invoke("Generate a list of 10 fake people information. Only return the list. Each person must have first name, last name and date of birth.")
+
+parser = PydanticOutputParser(pydantic_object=PeopleList)
+
+prompt = PromptTemplate(template="Answer the user query. \n {format_instructions} \n {query}",
+                        input_variables=["query"],
+                        partial_variables={"format_instructions": parser.get_format_instructions()})
+
+parser_input = prompt.format_prompt(query=people_data)
+model = ChatOpenAI()
+output = model.invoke(parser_input.to_string())
+
+people_data_parsed = parser.parse(output.content)
+print(people_data_parsed)
